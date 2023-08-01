4d98ea3af108807d18e57ec4f00d4f8322188717
smarthome <smarthome>
2023-08-01 23:24:14 +0200

    Line Creator added

--- FamilyTree/ArcCreator.cs
+++ FamilyTree/ArcCreator.cs
@@ -11,19 +11,19 @@ namespace FamilyTree
 		{
 			PointF p1 = new PointF
 			{
-				X = centre.X - radius * MathF.Sin(startAngle * MathF.PI / 180.0f),
+				X = centre.X + radius * MathF.Sin(startAngle * MathF.PI / 180.0f),
 				Y = centre.Y - radius * MathF.Cos(startAngle * MathF.PI / 180.0f)
 			};
 			PointF p2 = new PointF
 			{
-				X = centre.X - radius * MathF.Sin(endAngle * MathF.PI / 180.0f),
+				X = centre.X + radius * MathF.Sin(endAngle * MathF.PI / 180.0f),
 				Y = centre.Y - radius * MathF.Cos(endAngle * MathF.PI / 180.0f)
 			};
 
 			var totalAngle = endAngle - startAngle;
 			SvgPathSegmentList ls = new SvgPathSegmentList();
 			ls.Add(new SvgMoveToSegment(false, p1));
-			ls.Add(new SvgArcSegment(radius, radius, totalAngle, totalAngle < 180.0 ? SvgArcSize.Small : SvgArcSize.Large, SvgArcSweep.Negative, false, p2));
+			ls.Add(new SvgArcSegment(radius, radius, totalAngle, totalAngle < 180.0 ? SvgArcSize.Small : SvgArcSize.Large, SvgArcSweep.Positive, false, p2));
 
 			var path = new SvgPath
 			{
---
+++ FamilyTree/LineCreator.cs
@@ -0,0 +1,34 @@
+﻿using Svg;
+using Svg.Pathing;
+using System;
+using System.Drawing;
+
+namespace FamilyTree
+{
+	public static class LineCreator
+	{
+		public static SvgElement CreateLine(PointF centre, float startRadius, float endRadius, float angle, Color color)
+		{
+			PointF p1 = new PointF
+			{
+				X = centre.X + startRadius * MathF.Sin(angle * MathF.PI / 180.0f),
+				Y = centre.Y - startRadius * MathF.Cos(angle * MathF.PI / 180.0f)
+			};
+			PointF p2 = new PointF
+			{
+				X = centre.X + endRadius * MathF.Sin(angle * MathF.PI / 180.0f),
+				Y = centre.Y - endRadius * MathF.Cos(angle * MathF.PI / 180.0f)
+			};
+
+			var line = new SvgLine();
+			line.StartX = p1.X;
+			line.StartY = p1.Y;
+			line.EndX = p2.X;
+			line.EndY = p2.Y;
+			line.Stroke = new SvgColourServer(color);
+			line.StrokeWidth = Constants.StrokeWidth;
+
+			return line;
+		}
+	}
+}

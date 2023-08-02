7dde3a91b4621f09f0919c8898e247cc9adae4df
smarthome <smarthome>
2023-08-02 19:06:46 +0200

    CirclePlotter uses Colors

--- FamilyTree/CirclePlotter.cs
+++ FamilyTree/CirclePlotter.cs
@@ -1,5 +1,4 @@
 ﻿using Svg;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -11,10 +10,13 @@ namespace FamilyTree
 		public static void PlotCircles(SvgDocument document, PointF centre, int numberOfGenerations)
 		{
 			var radii = getCreateRadii(numberOfGenerations).ToArray();
-			document.Children.Add(ArcCreator.CreateCircle(centre, radii[0], Color.Black));
+			document.Children.Add(ArcCreator.CreateCircle(centre, radii[0], Color.FromArgb(255, 219, 219, 217)));
 			foreach (var radius in radii.Skip(1))
 			{
-				document.Children.Add(ArcCreator.CreateArcBorder(centre, radius, -Constants.OuterAngle, Constants.OuterAngle, Color.Black));
+				document.Children.Add(ArcCreator.CreateArcBorder(centre, radius, -Constants.OuterAngle, -Constants.OuterAngle/2.0f, Color.FromArgb(255, 44, 195, 242)));
+				document.Children.Add(ArcCreator.CreateArcBorder(centre, radius, -Constants.OuterAngle / 2.0f, 0, Color.FromArgb(255, 116, 158, 56)));
+				document.Children.Add(ArcCreator.CreateArcBorder(centre, radius, 0, Constants.OuterAngle / 2.0f, Color.FromArgb(255, 217, 84, 67)));
+				document.Children.Add(ArcCreator.CreateArcBorder(centre, radius, Constants.OuterAngle / 2.0f, Constants.OuterAngle, Color.FromArgb(255, 242, 201, 35)));
 			}
 		}
 

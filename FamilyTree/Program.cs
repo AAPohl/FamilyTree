469c59bc1e3ec08b1fb70c8c4228854efbbadef8
smarthome <smarthome>
2023-08-03 20:27:50 +0200

    Program.cs

---
+++ FamilyTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using Svg;
+using Svg.Pathing;
+
+namespace FamilyTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+	}
+}

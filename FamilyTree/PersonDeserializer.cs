724b83f149db411ba0360f82b3f04e6c08cc8f9d
smarthome <smarthome>
2023-08-28 20:08:29 +0200

    Serialization fix

--- FamilyTree/PersonDeserializer.cs
+++ FamilyTree/PersonDeserializer.cs
@@ -11,12 +11,12 @@ namespace FamilyTree
 
 			var name = root.Attribute("Name").Value;
 			var familiyName = root.Attribute("FamilyName").Value;
-			int.TryParse(root.Attribute("Birth")?.Value, out var birthYear);
-			int.TryParse(root.Attribute("Death")?.Value, out var deathYear);
+			var hasBirth = int.TryParse(root.Attribute("Birth")?.Value, out var birthYear);
+			var hasDeath = int.TryParse(root.Attribute("Death")?.Value, out var deathYear);
 			bool.TryParse(root.Attribute("IsAlive")?.Value, out var isAlive);
 			var father = Deserialize(root.Element("Father"));
 			var mother = Deserialize(root.Element("Mother"));
-			return new Person(name, familiyName, birthYear, deathYear, isAlive, father, mother);
+			return new Person(name, familiyName, hasBirth ? birthYear:null, hasDeath ? deathYear : null, isAlive, father, mother);
 		}
 	}
 }
